560a0577c371c8ad72018445496affb0fd6fe58f
MateusMello1995 <mellowmateus@outlook.com>
2020-02-01 08:25:52 -0300

    Initial commit

---
+++ app/src/main/java/com/example/webservices/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.webservices;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.concurrent.ExecutionException;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnBuscarCep = findViewById(R.id.btnMain_buscarCep);
+
+        final EditText cep = findViewById(R.id.etMain_cep);
+        final TextView resposta = findViewById(R.id.etMain_resposta);
+
+        btnBuscarCep.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    CEP retorno = new HttpService(cep.getText().toString()).execute().get();
+                    resposta.setText(retorno.toString());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+    }
+}
